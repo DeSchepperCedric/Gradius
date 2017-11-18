d867198a6de67c97218800037ff27011bc7ac659
Cedric De Schepper <Cedric@iMac-van-Cedric-De-Schepper.local>
2017-11-18 13:48:15 +0100

    changed cmake (src/build/bin). still needs some changes for cmakefiles

---
+++ build/src/cmake_install.cmake
@@ -0,0 +1,65 @@
+# Install script for directory: /Users/Cedric/Documents/CloudStation/School/GP/Gradius/src
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin/Gradius")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin" TYPE EXECUTABLE FILES "/Users/Cedric/Documents/CloudStation/School/GP/Gradius/build/src/Gradius")
+  if(EXISTS "$ENV{DESTDIR}/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin/Gradius" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin/Gradius")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Library/Developer/CommandLineTools/usr/bin/strip" "$ENV{DESTDIR}/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin/Gradius")
+    endif()
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin/Gradius")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin" TYPE EXECUTABLE FILES "/Users/Cedric/Documents/CloudStation/School/GP/Gradius/build/src/Gradius")
+  if(EXISTS "$ENV{DESTDIR}/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin/Gradius" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin/Gradius")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Library/Developer/CommandLineTools/usr/bin/strip" "$ENV{DESTDIR}/Users/Cedric/Documents/CloudStation/School/GP/Gradius/bin/Gradius")
+    endif()
+  endif()
+endif()
+
